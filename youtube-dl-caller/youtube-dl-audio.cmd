5dcb0742789b9747f0ad33f35b5914a22593b475
h241035 <luigidamiano.russo@bancaditalia.it>
2024-01-04 12:00:58 +0100

    - yt-dlp nuova versione con funzionalità estese - manuale yt-dlp - menu batch con link preconfigurati

--- youtube-dl-caller/youtube-dl-audio.cmd
+++ youtube-dl-caller/youtube-dl-audio.cmd
@@ -1,47 +1,109 @@
 @ECHO OFF
 CLS
 COLOR 7C
+ECHO.
+ECHO.
 ECHO     YOUTUBE AUDIO DOWNLOADER
 ECHO.
 ECHO ======================================================================================================================
 ECHO.
-SET /P url="[Enter video URL/list]: "
+SET /P url="[Enter video URL/list or type <m> for menu ]: "
+ECHO %url%
+IF /i "%url%" == "m" (GOTO menu)
+GOTO nuovo
+
+
+:nuovo
 ECHO.
-GOTO folder
+ECHO.
+ECHO.
+ECHO.
+SET /P folder="[Type folder to save to ]: "
+ECHO.
+PAUSE
+GOTO network
 
 rem testUrl=https://www.youtube.com/watch?v=CMKHQxvV7r8
+rem myurl=https://www.youtube.com/playlist?list=PL9u4YZN18435tu1jPn9wwwahgBqe_Q4QW
 
 
-:folder
+:menu
 ECHO.
-SET /P folder="      Type folder to save to: "
+ECHO    1) Ecstatic Dance   - 'ecstatic'
+ECHO    2) Disco/Techno     - 'truzza'
+ECHO    3) Lindy Hop        - 'swing'
+ECHO    4) Balboa           - 'balboa'
+ECHO    5) Tango            - 'tango'
 ECHO.
-GOTO selection
+ECHO.
+SET /P menu="[Enter predefined list ]: "
+ECHO.
+ECHO. 
+IF %menu% == 1 (GOTO ecstatic)
+IF %menu% == 2 (GOTO truzza)
+IF %menu% == 3 (GOTO lindyhop)
+IF %menu% == 4 (GOTO balboa)
+IF %menu% == 5 (GOTO tango)
+ECHO.
+ECHO   Error: Unknown value
+GOTO menu
 
 
-:selection
+:ecstatic
+SET url=https://www.youtube.com/playlist?list=PL9u4YZN18435tu1jPn9wwwahgBqe_Q4QW
+SET folder=ecstatic
+GOTO network
+
+:truzza
+SET url=https://www.youtube.com/playlist?list=PL9u4YZN18436KADHpnqYCJaw3CB6vgS-d
+SET folder=truzza
+GOTO network
+
+:lindyhop
+SET url=https://www.youtube.com/playlist?list=PL9u4YZN18434gzMo2OU795uzOYkpVUtjq
+SET folder=swing
+GOTO network
+
+:balboa
+SET url=https://www.youtube.com/playlist?list=PL9u4YZN18437kdgtXO3HnITxpyuBq4bOp
+SET folder=balboa
+GOTO network
+
+:tango
+SET url=https://www.youtube.com/playlist?list=PL9u4YZN18434NO499gZWSscqvbfYFcBCP
+SET folder=tango
+GOTO network
+
+
+
+:network
 ECHO.
-ECHO       1) Direct link
-ECHO       2) With proxy
+ECHO    1) Direct link
+ECHO    2) With proxy
 ECHO.
-SET /P option="      Select option: "
+SET /P option="[Select connection mode ]: "
 IF %option% == 1 (GOTO direct)
 IF %option% == 2 (GOTO withProxy)
 ECHO.
 ECHO   Error: Unknown value
-GOTO selection
-
+GOTO network
 
 
 
 :direct
 ECHO.
-youtube-dl.exe --config-location ./youtube-dl-audio.conf -o "./%folder%/%%(title)s.%%(ext)s" %url%
+ECHO.
+ECHO Saving into '%folder%' from '%url%':
+ECHO.
+yt-dlp.exe --no-check-certificate --config-locations ./youtube-dl-audio.conf -o d:/musica/%folder%/%%(title)s.%%(ext)s %url%
 GOTO fin
 
 :withProxy
 ECHO.
-youtube-dl.exe --proxy http://utenze\h241035:Giu0606giu@itaca-prod.utenze.bankit.it:8080/ --config-location ./youtube-dl-audio.conf -o ./%folder%/%%(title)s.%%(ext)s %url%
+ECHO.
+ECHO Saving into '%folder%' from '%url%':
+ECHO.
+yt-dlp.exe --no-check-certificate --proxy http://utenze\***ute***:****pwd*****@itaca-prod.utenze.bankit.it:8080/ --config-locations ./youtube-dl-audio.conf -o d:/musica/%folder%/%%(title)s.%%(ext)s %url%
 GOTO fin
 
 :fin
